5715fd3f3947887b5c95e7e2e5652271edcc2f86
Max van der Graaff <max.graaff@gmail.com>
2019-03-22 13:09:33 +0100

    BT_36 Updated Request Direct Move: Crossproduct of the AI forward intent and the safe normal of the tank.

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -30,9 +30,11 @@ void UTankMovementComponent::IntentTurnRight(float Throw)
 
 void UTankMovementComponent::RequestDirectMove(const FVector & MoveVelocity, bool bForceMaxSpeed)
 {
-	auto TankName = GetOwner()->GetName();
-	auto MoveVelocityString = MoveVelocity.ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"), *TankName, *MoveVelocityString);
+	auto TankForWard = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+	auto ForwardThrow = FVector::DotProduct(AIForwardIntention, TankForWard);
+
+	IntentMoveFoward(ForwardThrow);
 }
 
 
